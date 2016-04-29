d81c11a2afdb5d3ee205c705908339ed83cfbf31
unknown <kenneththieu21@gmail.com>
2016-04-28 18:01:39 -0700

    android lib and ads

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,20 +1,26 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
+import com.udacity.gradle.builditbigger.JokeSmith;
+import com.udacity.gradle.builditbigger.jokedisplay.JokeActivity;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    private static Toast mToast;
     public MainActivityFragment() {
     }
 
@@ -22,6 +28,26 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button jokeButton = (Button) root.findViewById(R.id.joke_telling_button);
+        jokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JokeSmith smith = new JokeSmith();
+                Intent sendIntent = new Intent(getActivity(), JokeActivity.class);
+                sendIntent.putExtra(JokeActivity.TAG, smith.getJoke());
+                startActivity(sendIntent);
+                /*
+                if(mToast != null) {
+                    mToast.cancel();
+                    mToast = null;
+                }
+
+                mToast = Toast.makeText(getActivity(), smith.getJoke(), Toast.LENGTH_SHORT);
+                mToast.show();
+                */
+            }
+        });
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
--- jokedisplay/src/main/java/com/udacity/gradle/builditbigger/jokedisplay/JokeActivity.java
+++ jokedisplay/src/main/java/com/udacity/gradle/builditbigger/jokedisplay/JokeActivity.java
@@ -1,30 +1,30 @@
-package com.udacity.gradle.builditbigger;
+package com.udacity.gradle.builditbigger.jokedisplay;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
+public class JokeActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
+    public static String TAG = "joke_tag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_joke);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_joke, menu);
         return true;
     }
 
@@ -43,10 +43,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /*
-    public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
-    }
-    */
 
 }
